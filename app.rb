20c4daa55f1cc445817af361218e891a88ab7f29
emccallum <mccallum.ek@gmail.com>
2014-06-29 18:19:29 -0700

    Adding database configuration and models erb

--- app.rb
+++ app.rb
@@ -1,11 +1,22 @@
 require 'sinatra'
 require 'sinatra/activerecord'
 require './config/environments'
+require './models/model'
 
 get '/' do 
     erb :index
 end
 
-post '/s/take' do
-    redirect '/'
+post '/submit' do
+    @model = Model.new(params[:model])
+    if @model.save
+        redirect '/models'
+    else
+        "Sorry, there was an error!"
+    end
+end
+
+get '/models' do
+    @models = Model.all
+    erb :models
 end
