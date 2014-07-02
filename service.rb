16a9a8d4d3f9413e3b7799b36fe8dc5d3c9f62ac
Jen Scobie <jenscobie@gmail.com>
2014-07-01 20:07:45 -0700

    Added a failing test for survey post

--- service.rb
+++ service.rb
@@ -2,8 +2,14 @@ require 'sinatra'
 require 'json'
 require './models/survey'
 
-get '/survey/:id' do |id|
-  content_type :json
-  survey = Survey.new(id, '1234 California Street')
-  survey.to_json
+get '/surveys/survey/:id' do |id|
+	content_type :json
+	survey = Survey.new(id, '1234 California Street')
+	survey.to_json
+end
+
+post '/surveys' do
+	content_type :json
+	survey = Survey.new('blah', '543 test street')
+	survey.to_json
 end
--- specs/survey_spec.rb
+++ specs/survey_spec.rb
@@ -3,15 +3,28 @@ require 'rest_client'
 require './models/survey'
 require 'json'
 
-base_url = 'http://localhost:4567/survey/blee'
+surveys_endpoint = 'http://localhost:4567/surveys'
+survey_endpoint = 'http://localhost:4567/surveys/survey/blee'
 
 describe 'survey service' do
-   it "serves up json data" do
-    response = RestClient.get base_url
-    expect(response.code).to eq(200)
-   
-    survey = Survey.json_create(JSON.parse(response.body))
-    expected_survey = Survey.new('blee', '1234 California Street')
-    expect(survey).to eq(expected_survey)
-  end
+
+	it "serves up json data" do
+		response = RestClient.get survey_endpoint
+		expect(response.code).to eq(200)
+
+		survey = Survey.json_create(JSON.parse(response.body))
+		expected_survey = Survey.new('blee', '1234 California Street')
+		expect(survey).to eq(expected_survey)
+	end
+
+	it "saves survey data posted to the surveys endpoint" do
+		expected_survey = Survey.new('one', '321 test street')
+		RestClient.post surveys_endpoint, { 'x' => 1 }.to_json, :content_type => :json, :accept => :json
+
+		response = RestClient.get survey_endpoint
+		expect(response.code).to eq(200)
+
+		survey = Survey.json_create(JSON.parse(response.body))
+		expect(survey).to eq(expected_survey)
+	end
 end
